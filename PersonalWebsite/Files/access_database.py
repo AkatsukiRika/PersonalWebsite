cacd05012f185c2c0c7dc38577c328bd3789fd7c
AkatsukiRika <m1345860061@gmail.com>
2020-01-21 16:03:21 +0800

    January 21th

--- PersonalWebsite/Files/access_database.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-con = sqlite3.connect('db.sqlite3')
-print("Database connected!")
-print("Please input your SQL statement:")
-sql = input()
-cur = con.execute(sql)
-print(cur.fetchall())
